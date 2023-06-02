a628295fc3f91ee7e4a6c00552b9429e0abb9155
MisiekWieviorka <misiek.wieviorka12@gmail.com>
2023-06-02 16:08:15 +0200

    Stworzono wybór działania przez użytkownika w mainie

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args) {
@@ -23,5 +24,62 @@ public class Main {
 
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
+        while (true)
+        {
+            System.out.println("1.Dodaj pracownika");
+            System.out.println("2.Usuń pracownika");
+            System.out.println("3.Zaktualizuj dane o pracowniku");
+            System.out.println("4.Wyszukaj pracownika po ID");
+            System.out.println("5.Wyświetl liste wszystkich pracownikow");
+            System.out.println("6.Wyszukaj pracownika po pozycji");
+            System.out.println("Inna wartosc zakonczy program");
+            int input = Integer.parseInt(bufferedReader.readLine());
+            switch (input){
+                case 1:
+                    menadzerPracownikow.dodajPracownika(pracownik7);
+                    break;
+                case 2:
+                {
+                    int id = Integer.parseInt(bufferedReader.readLine());
+                    menadzerPracownikow.usunPracownika(menadzerPracownikow.znajdzPracownikaPrzezId(id));
+                    break;
+                }
+                case 3:
+                {
+                    int id = Integer.parseInt(bufferedReader.readLine());
+                    Pracownik pracownik = menadzerPracownikow.znajdzPracownikaPrzezId(id);
+                    if(pracownik != null){
+                        pracownik.setPensja(10000);
+                        menadzerPracownikow.zaktualizujPracownika(id,pracownik);}
+                    break;
+
+                }
+                case 4:
+                {
+                    int id = Integer.parseInt(bufferedReader.readLine());
+                    Pracownik pracownik = menadzerPracownikow.znajdzPracownikaPrzezId(id);
+                    System.out.println(pracownik);
+                    break;
+                }
+                case 5:
+                {
+                    menadzerPracownikow.listaWszystkichPracowników();
+                    break;
+                }
+                case 6:
+                {
+                    String pozycja = bufferedReader.readLine();
+                    HashSet<Pracownik> employeeHashSet = menadzerPracownikow.szukajPracownikowPrzezPozycje(pozycja);
+                    for (Pracownik pracownik: employeeHashSet
+                    ) {
+                        System.out.println(pracownik);
+                    }
+                    break;
+
+                }
+                default:
+                    System.exit(0);
+            }
+        }
     }
 }
