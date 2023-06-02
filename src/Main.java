8164c77ceb41c064ec1194084c80ffb4da878f77
MisiekWieviorka <misiek.wieviorka12@gmail.com>
2023-06-02 15:07:41 +0200

    Stworzono ośmiu pracowników w mainie

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Pracownik pracownik1 = new Pracownik("John", "Doe", "Manager", 1, 75000);
+        Pracownik pracownik2 = new Pracownik("Jane", "Smith", "Accountant", 2, 60000);
+        Pracownik pracownik3 = new Pracownik("Michael", "Johnson", "Accountant", 3, 55000);
+        Pracownik pracownik4 = new Pracownik("Emily", "Jones", "Marketing Specialist", 4, 50000);
+        Pracownik pracownik5 = new Pracownik("David", "Brown", "Accountant", 5, 45000);
+        Pracownik pracownik6 = new Pracownik("Laura", "Taylor", "Human Resources Manager", 6, 70000);
+        Pracownik pracownik7 = new Pracownik("Kevin", "Wilson", "IT Specialist", 7, 60000);
+        Pracownik pracownik8 = new Pracownik("Samantha", "Davis", "Customer Service Representative", 8, 40000);
+
     }
 }
