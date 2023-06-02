0719d114437a761f5529ceca24309a41a508f698
MisiekWieviorka <misiek.wieviorka12@gmail.com>
2023-06-02 14:54:51 +0200

    Stworzono zmienną pracownicy i konstruktor w klasie MenadzerPracownikow

--- src/MenadzerPracownikow.java
+++ src/MenadzerPracownikow.java
@@ -1,3 +1,10 @@
+import java.util.HashMap;
+
 public class MenadzerPracownikow {
 
+    private final HashMap<Integer,Pracownik> pracownicy;
+
+    public MenadzerPracownikow(HashMap<Integer, Pracownik> pracownicy) {
+        this.pracownicy = pracownicy;
+    }
 }
